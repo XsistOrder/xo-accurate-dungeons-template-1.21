d0c436dbc1d381efbf146002e3be09f6c8fd5184
XsistOrder <mbecerrafletcher@hotmail.com>
2024-12-03 22:18:18 -0500

    beanzzz

---
+++ src/main/java/net/xo/xoaccuratedungeons/XOAccurateDungeonsClient.java
@@ -0,0 +1,10 @@
+package net.xo.xoaccuratedungeons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XOAccurateDungeonsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/xo/xoaccuratedungeons/XOAccurateDungeonsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.xo.xoaccuratedungeons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class XOAccurateDungeonsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
