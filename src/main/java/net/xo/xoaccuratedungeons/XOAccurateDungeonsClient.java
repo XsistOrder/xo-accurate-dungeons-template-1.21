5b3e3bfc5f65eb817f6ab2187fa6ead435c56215
XsistOrder <mbecerrafletcher@hotmail.com>
2025-03-08 13:13:04 -0500

    new block and test trees

--- src/main/java/net/xo/xoaccuratedungeons/XOAccurateDungeonsClient.java
+++ src/main/java/net/xo/xoaccuratedungeons/XOAccurateDungeonsClient.java
@@ -1,10 +1,14 @@
 package net.xo.xoaccuratedungeons;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.xo.xoaccuratedungeons.block.ModBlocks;
 
 public class XOAccurateDungeonsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CAGE_BLOCK, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.INVERTED_SAPLING, RenderLayer.getCutout());
     }
 }
